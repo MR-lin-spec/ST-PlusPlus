1314d690fca114e9f82a800b81f3e6b09b799d1f
MDJL <3304452595@qq.com>
2025-11-03 13:09:01 +0800

    对第一次测试的抵消（返回旧版本）

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from model.semseg.deeplabv3plus import DeepLabV3Plus
 from model.semseg.pspnet import PSPNet
 from utils import count_params, meanIOU, color_map
 
-#Test_new测试版本
-#Test_new第二次测试版本
 import argparse
 from copy import deepcopy
 import numpy as np
